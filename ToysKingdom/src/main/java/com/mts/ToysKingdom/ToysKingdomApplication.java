548733c8364a71a78de9322c1d5e6ac8119ee51e
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-05-16 09:05:42 +0700

    Startup project

---
+++ ToysKingdom/src/main/java/com/mts/ToysKingdom/ToysKingdomApplication.java
@@ -0,0 +1,13 @@
+package com.mts.ToysKingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysKingdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysKingdomApplication.class, args);
+	}
+
+}
---
+++ ToysKingdom/src/test/java/com/mts/ToysKingdom/ToysKingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.ToysKingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysKingdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
