a853bfa9b0fc93bda06431981f85db75f2c1662f
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-05-22 13:20:08 +0700

    làm lại

--- ToysKingdom/src/main/resources/static/assets/js/main.js
+++ ToysKingdom/src/main/resources/static/assets/js/main.js
@@ -2,12 +2,28 @@ let app = angular.module('ToysKingdom', ["ngRoute"]);
 
 app.config(function ($routeProvider) {
     $routeProvider
-    .when("/login", { templateUrl: "./login.html", controller: "myCtrl" })
-    .when("/siginUp", { templateUrl: "./SignUp.html", controller: "myCtrl" })
-    .otherwise({ templateUrl: "page/ViewProduct.html", controller: "mainCtrl" });
+        .when("/", {templateUrl: "page/ViewProduct.html", controller: "mainCtrl"})
+        .when("/siginUp", {templateUrl: "./SignUp.html", controller: "myCtrl"})
+        .otherwise({templateUrl: "layout/error.html", controller: "myCtrl"});
 });
 
 app.controller('mainCtrl', function ($scope, $http) {
+    $scope.productsData = []
+
+    const getData = async () => {
+        await $http.get('http://localhost:8080/api-public/products/getAllActiveProducts')
+            .then(function (response){
+                $scope.productsData = response.data.data;
+                console.log('getAllActiveProducts')
+            }).catch(function (error) {
+            console.log(error)
+
+        })
+    }
+
+    getData();
+
+
 
 })
 
