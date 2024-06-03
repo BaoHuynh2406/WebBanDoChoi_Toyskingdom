a05824da35005d8471ca842e81b960f81fa2b9fc
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-06-03 13:16:12 +0700

    fix bug

--- ToysKingdom/src/main/resources/static/assets/js/app/Search/Search.controller.js
+++ ToysKingdom/src/main/resources/static/assets/js/app/Search/Search.controller.js
@@ -1,25 +1,22 @@
-angular.module('ToysKingdom').controller('searchCtrl', function ($scope ,$location, $http, $routeParams) {
+angular.module('ToysKingdom').controller('searchCtrl', function ($scope, $location, $http, $routeParams) {
+    console.log('Search controller load');
     $scope.productsData = [];
-    $scope.searchKey = $routeParams.query || '';
+    $scope.searchQuery = $routeParams.query || '';
 
     $scope.loadData = function () {
-        console.log("Loaddata...");
-        if ($scope.searchKey) {
-            $http.get('http://localhost:8080/api-public/products/getproductbyname', { params: { productName: $scope.searchKey } })
-                .then(function (response) {
-                    $scope.productsData = response.data.data;
-                }, function (error) {
-                    console.log(error.message);
-                });
-        } else {
-            console.log("Vui lòng nhập sản phẩm cần tìm kiếm");
-        }
+        $http.get('http://localhost:8080/api-public/products/getproductbyname', { params: { productName: $scope.searchQuery } })
+            .then(function (response) {
+                $scope.productsData = response.data.data;
+            }, function (error) {
+                console.log(error.message);
+            });
     };
 
     // Khi khởi tạo controller, nếu có tham số query trên URL thì gán vào thanh tìm kiếm và gọi loadData
-    if ($scope.searchKey) {
+    if ($scope.searchQuery) {
+        $("#searchQuery").val($scope.searchQuery);
         $scope.loadData();
     }
 
-    console.log('Search controller load')
+
 });
--- ToysKingdom/src/main/resources/static/assets/js/app/app.js
+++ ToysKingdom/src/main/resources/static/assets/js/app/app.js
@@ -1,11 +1,13 @@
-angular.module('ToysKingdom').controller('mainCtrl', function ($scope){
+angular.module('ToysKingdom').controller('mainCtrl', function ($scope, $location){
     console.log("MainCtrl Load Done")
+    
     $scope.search = function () {
-        console.log($scope.searchKey);
-        if ($scope.searchKey) {
-            console.log("Searching for:", $scope.searchKey);
+        $scope.searchQuery = $('#searchQuery').val();
+        console.log($scope.searchQuery);
+        if ($scope.searchQuery) {
+            console.log("Searching for:", $scope.searchQuery);
             // Gán nội dung người dùng tìm kiếm lên URL
-            $location.path('/search').search({ query: $scope.searchKey });
+            $location.path('/search').search({ query: $scope.searchQuery });
         } else {
             console.log("Product name cannot be null or empty");
         }
