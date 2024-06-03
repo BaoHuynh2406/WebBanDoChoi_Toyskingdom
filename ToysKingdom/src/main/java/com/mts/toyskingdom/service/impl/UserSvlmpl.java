dd058c6c93fd45fa66b46e15f5ff6eb25b3a51d9
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-06-03 21:59:50 +0700

    fix api user

--- ToysKingdom/src/main/java/com/mts/toyskingdom/service/impl/UserSvlmpl.java
+++ ToysKingdom/src/main/java/com/mts/toyskingdom/service/impl/UserSvlmpl.java
@@ -1,44 +1,38 @@
 package com.mts.toyskingdom.service.impl;
 
 import com.mts.toyskingdom.data.entity.UserE;
-import com.mts.toyskingdom.data.model.ProductM;
 import com.mts.toyskingdom.data.model.UserM;
 import com.mts.toyskingdom.mapper.UserMapper;
 import com.mts.toyskingdom.service.UserSv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserSvlmpl implements UserSv {
-final UserMapper userMapper;
+    final UserMapper userMapper;
 
     @Override
-    public List<UserM> getUserByidUser(int idUser) throws SQLException {
-        var listResultEntity = userMapper.getUserByidUser(idUser);
-        if (Objects.nonNull(listResultEntity)) {
-            return UserM.convertListUserEToUserM(listResultEntity);
-        }
-        return null;
+    public List<UserM> getUserByidUser(int idUser) {
+        return convertList(userMapper.getUserByidUser(idUser));
     }
 
     @Override
-    public List<UserM> getAllUser()  throws SQLException{
-        var listResultEntity = userMapper.getAllUser();
-        if (Objects.nonNull(listResultEntity)) {
-            return UserM.convertListUserEToUserM(listResultEntity);
-        }
-        return null;
+    public List<UserM> getAllUser() {
+        return convertList(userMapper.getAllUser());
     }
+
     @Override
-    public List<UserM> getUserByEmail(String email)  throws SQLException{
-        var listResultEntity = userMapper.getUserByEmail(email);
-        if (Objects.nonNull(listResultEntity)) {
-            return UserM.convertListUserEToUserM(listResultEntity);
-        }
-        return null;
+    public List<UserM> getUserByEmail(String email) {
+        return convertList(userMapper.getUserByEmail(email));
+    }
+
+    private List<UserM> convertList(List<UserE> listResultEntity) {
+        return Optional.ofNullable(listResultEntity)
+                .map(UserM::convertListUserEToUserM)
+                .orElse(null);
     }
 }
