f3b3bd2e50b2217cdc9d2e1b8b028aac18388791
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-06-05 12:44:10 +0700

    Reapply "fix api user"
    This reverts commit a2b2d8a95439751b947d90c282c0a86e7247d078.

--- ToysKingdom/src/main/java/com/mts/toyskingdom/data/entity/UserE.java
+++ ToysKingdom/src/main/java/com/mts/toyskingdom/data/entity/UserE.java
@@ -7,8 +7,8 @@ import java.util.Date;
 public class UserE {
 
     private int idUser;
-    private String password;
     private String email;
+    private String password;
     private String phoneNumber;
     private String address;
     private Date birthday;
--- ToysKingdom/src/main/java/com/mts/toyskingdom/data/model/UserM.java
+++ ToysKingdom/src/main/java/com/mts/toyskingdom/data/model/UserM.java
@@ -3,6 +3,7 @@ package com.mts.toyskingdom.data.model;
 import com.mts.toyskingdom.data.entity.UserE;
 import lombok.*;
 
+import java.util.Date; // Thêm import này để sử dụng trường birthday
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,17 +15,24 @@ import java.util.stream.Collectors;
 public class UserM {
 
     private int idUser;
-    private String password;
     private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private java.sql.Date birthday;
     private String role;
-
+    private boolean active;
 
     public static UserM convertUserEToUserM(UserE userE) {
         return UserM.builder()
                 .idUser(userE.getIdUser())
-                .password(userE.getPassword())
                 .email(userE.getEmail())
+                .password(userE.getPassword())
+                .phoneNumber(userE.getPhoneNumber())
+                .address(userE.getAddress())
+                .birthday(new java.sql.Date(userE.getBirthday().getTime()))
                 .role(userE.getRole())
+                .active(userE.isActive())
                 .build();
     }
 
