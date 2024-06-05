63f3458c334d55001ef35607afb320f312023258
Kiet <trantrankiet59@gmail.com>
2024-06-05 21:20:26 +0700

    Revert "insertUser"
    This reverts commit 866b1862b5a836b5beffd7fcf2454ef71280c84d.

--- ToysKingdom/src/main/java/com/mts/toyskingdom/mapper/UserMapper.java
+++ ToysKingdom/src/main/java/com/mts/toyskingdom/mapper/UserMapper.java
@@ -1,8 +1,6 @@
 package com.mts.toyskingdom.mapper;
 
-import com.mts.toyskingdom.data.dto.UserRegistrationDto;
 import com.mts.toyskingdom.data.entity.UserE;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,5 +10,5 @@ public interface UserMapper {
 public List<UserE> getAllUser();
 public List<UserE> getUserByEmail(@Param("email") String email);
 public List<UserE> getUserByidUser(@Param("idUser") int idUser);
-public int insertUser(UserRegistrationDto UserRegistration);
+
 }
