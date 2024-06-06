b43390c7e7ad9fd9a2927aab070887ad27c04eac
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-06-06 20:09:06 +0700

    loi cai gi do

--- ToysKingdom/src/main/java/com/mts/toyskingdom/mapper/UserMapper.java
+++ ToysKingdom/src/main/java/com/mts/toyskingdom/mapper/UserMapper.java
@@ -6,11 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+
 @Mapper
 public interface UserMapper {
-public List<UserE> getAllUser();
-public List<UserE> getUserByEmail(@Param("email") String email);
-public List<UserE> getUserByidUser(@Param("idUser") int idUser);
+    public List<UserE> getAllUser();
+
+    public List<UserE> getUserByEmail(@Param("email") String email);
+
+    public List<UserE> getUserByidUser(@Param("idUser") int idUser);
 
     public int insertUser(UserRegistrationDto UserRegistration);
 
