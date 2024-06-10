56a19df50be9e3943e79ab2aee882abe7fa86f03
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-06-10 13:26:35 +0700

    hoan tat

--- ToysKingdom/src/main/resources/static/admin/js/app/thongke/doanhthu.controller.js
+++ ToysKingdom/src/main/resources/static/admin/js/app/thongke/doanhthu.controller.js
@@ -1,22 +1,17 @@
 angular.module('admin')
-    .controller('doanhthuCtrl',
-        function ($scope) {
+    .controller('doanhthuCtrl', 
+        function ($scope, $http) {
             console.log('Đã tải trang doanh thu')
-            $scope.tongDoanhThu = 0;
+       
             $scope.orders = [];
 
             $scope.getAll = function () {
                 var button = document.getElementById("rotate-button");
                 button.classList.add("rotate-icon");
-                $http.get('')
+                $http.get('/api-public/orders/getAllByStatus')
                     .then(function (response) {
                         $scope.orders = response.data.data;
                         button.classList.remove("rotate-icon");
-                        $scope.orders.forEach(element => {
-                            if(element.status === "PAID") {
-                                $scope.tongDoanhThu = $scope.tongDoanhThu + element.total;
-                            }
-                        });
                     })
                     .catch(function (error) {
                         console.error('Error loading items:', error);
